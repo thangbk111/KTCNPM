0cfb3d5d0c44e20fd3c807bc7485a76d99901700
thang.nv <thang.nv>
2018-04-11 23:20:25 +0700

    3.3 done

--- js/exercise31.js
+++ js/exercise31.js
@@ -9,17 +9,12 @@ $(document).ready(function() {
      return presentValue * Math.pow((1 + rate), month);
     }
 
-    //cash flow
-    function cashFlow(investment, cost) {
-     return investment - cost;
-    }
-
     function calculatePV(event){
         var $form = $(event.target).closest('form');
         var futureWorth = parseFloat($form.find('input[name="future_worth"]').val());
         var month = parseFloat($form.find('input[name="month"]').val());
         var rate = parseFloat($form.find('input[name="rate"]').val());
-        var PV = presentValue(futureWorth, rate, month);
+        var PV = presentValue(futureWorth, rate, month).toFixed(2);
         $('.prevent_value').text(PV + "$");
     }
 
@@ -28,12 +23,63 @@ $(document).ready(function() {
         var presentValue = parseFloat($form.find('input[name="present_value"]').val());
         var month = parseFloat($form.find('input[name="month"]').val());
         var rate = parseFloat($form.find('input[name="rate"]').val());
-        var FW = futureWorth(presentValue, rate, month);
+        var FW = futureWorth(presentValue, rate, month).toFixed(2);
         $('.future_worth').text(FW + "$");
     }
 
-    
+    function getAllProceeds(event) {
+        var proceeds = [];
+        var $table = $(event.target).closest('table');
+        $table.find('.proceed').each(function() {
+            proceeds.push(parseFloat($(this).val()));
+        });
+        return proceeds;
+    }
+
+    function getAllCosts(event) {
+        var costs = [];
+        var $table = $(event.target).closest('table');
+        $table.find('.cost').each(function() {
+            costs.push(parseFloat($(this).val()));
+        });
+        return costs;
+    }
+
+    function calculateCashFlow(proceeds, costs) {
+        var cashFlow = [];
+        for (var i = 0; i < costs.length; i++) {
+            cashFlow.push(proceeds[i] - costs[i]);
+        }
+        return cashFlow;
+    }
+
+    function calculatePVs(cashFlow, rate) {
+        var values = [];
+        for (var i = 0; i < cashFlow.length; i++) {
+            values.push(presentValue(cashFlow[i], rate, i));
+        }
+        return values;
+    }
+
+    function calculateNPV(event) {
+        var $table = $(event.target).closest('table');
+        var rate = parseFloat($table.find('.rate').val());
+        console.log(rate);
+        var proceeds = getAllProceeds(event);
+        var costs = getAllCosts(event);
+        var cashFlow = calculateCashFlow(proceeds, costs);
+        var presentValues = calculatePVs(cashFlow, rate);
+        var npv = 0;
+        for (var i = 0; i < presentValues.length; i++) {
+            npv += presentValues[i];
+            //genarate DOM
+            $table.find(".cash_flow").eq(i).text(cashFlow[i]);
+            $table.find(".present_value").eq(i).text(presentValues[i].toFixed(2));
+        }
+        $table.find('.npv').text(npv.toFixed(2) + "$");
+    }
     //Add event to DOM
     $('button[name="calculate_present_value"]').click(calculatePV);
     $('button[name="calculate_future_worth"]').click(calculateFW);
+    $('button[name="calculate_npv"]').click(calculateNPV);
 });
